94e36e1679bd3e1e1de1ebcdaa59f50ad6138488
Darren Spruell <phatbuckett@gmail.com>
2023-03-28 21:26:09 -0700

    Update for testing release.
    - Enable processing multiple input domains.
    - Add support for a sleep/pause between WHOIS queries (default 15s).
    - Add documentation and demo input file.

--- whois_format.py
+++ whois_format.py
@@ -1,12 +1,14 @@
-"""Whois client wrapper producing terse, single-line format."""
+"""Whois client wrapper producing a terse, single-line format."""
 
 import datetime
 import logging
 from argparse import ArgumentParser, FileType
+from time import sleep
 
 import pkg_resources
 from tabulate import tabulate
 from whois import whois  # type: ignore
+
 
 __application_name__ = "whois-format"
 __version__ = pkg_resources.get_distribution(__application_name__).version
@@ -15,6 +17,7 @@ __full_version__ = f"{__application_name__} {__version__}"
 logging.basicConfig(level=logging.DEBUG, format="[%(levelname)s] %(message)s")
 
 DEFAULT_STR = "-"
+NUM_SLEEP_SECONDS = 15
 
 
 def get_ns_domains(nameservers: list) -> list:
@@ -28,10 +31,22 @@ def get_ns_domains(nameservers: list) -> list:
 
 def cli():
     """CLI entry point."""
-    description = "Whois client wrapper producing terse, single-line format."
+    description = "Whois client wrapper producing a terse, single-line format."
     parser = ArgumentParser(description=description)
+    parser.add_argument(
+        "-s",
+        "--sleep",
+        type=int,
+        default=NUM_SLEEP_SECONDS,
+        help=(
+            "number of seconds to sleep for a pause between lookups of "
+            "multiple domains (default: %(default)s)"
+        ),
+    )
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument("-d", "--domain", help="domain name to query")
+    group.add_argument(
+        "-d", "--domain", nargs="+", help="domain name(s) to query"
+    )
     group.add_argument(
         "-f", "--in-file", type=FileType("r"), help="input file of domains"
     )
@@ -44,30 +59,41 @@ def cli():
     )
     args = parser.parse_args()
 
-    data = []
+    resp_data = []
 
-    w = whois(args.domain.lower())
-
-    # Field ordering matters - keep to this format:
-    # domain, creation_date, registrar, nameservers, registrant name or
-    # organization, registrant email(s)
-
-    data.append(w.domain.upper())
-    creation = w.get("creation_date")
-    if isinstance(creation, list):
-        dt = creation[0]
+    if args.in_file:
+        lookup_domains = args.in_file.readlines()
     else:
-        dt = creation
-    if isinstance(dt, datetime.datetime):
-        data.append(dt.strftime("%Y-%m-%d"))
-    else:
-        data.append(DEFAULT_STR)
-    data.append(w.get("registrar", DEFAULT_STR))
-    ns_list = get_ns_domains(w.get("name_servers", []))
-    data.append(", ".join(ns_list or ["-"]))
-    data.append(w.get("name") or w.get("org", DEFAULT_STR))
-    emails = w.get("emails", [DEFAULT_STR])
-    if not isinstance(emails, list):
-        emails = [emails]
-    data.append(", ".join(emails))
-    print(tabulate([data], tablefmt="plain"))
+        lookup_domains = args.domain
+
+    for domain in lookup_domains:
+        data = []
+        w = whois(domain.strip().lower())
+
+        # Field ordering matters - keep to this format:
+        # domain, creation_date, registrar, nameservers, registrant name or
+        # organization, registrant email(s)
+
+        data.append(w.domain.upper())
+        creation = w.get("creation_date")
+        if isinstance(creation, list):
+            dt = creation[0]
+        else:
+            dt = creation
+        if isinstance(dt, datetime.datetime):
+            data.append(dt.strftime("%Y-%m-%d"))
+        else:
+            data.append(DEFAULT_STR)
+        data.append(w.get("registrar", DEFAULT_STR))
+        ns_list = get_ns_domains(w.get("name_servers", []))
+        data.append(", ".join(ns_list or ["-"]))
+        data.append(w.get("name") or w.get("org", DEFAULT_STR))
+        emails = w.get("emails", [DEFAULT_STR])
+        if not isinstance(emails, list):
+            emails = [emails]
+        data.append(", ".join(emails))
+
+        resp_data.append(data)
+        sleep(args.sleep)
+
+    print(tabulate(resp_data, tablefmt="plain"))
